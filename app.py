f253ae6893d1baf3368884a1de1607b62f4af084
deglurrupesh-cpu <deglurrupesh@gmail.com>
2026-07-24 20:46:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import socket
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return jsonify({
@@ -10,11 +11,13 @@ def home():
         "hostname": socket.gethostname()
     })
 
+
 @app.route("/health")
 def health():
     return jsonify({
         "status": "healthy"
     }), 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
